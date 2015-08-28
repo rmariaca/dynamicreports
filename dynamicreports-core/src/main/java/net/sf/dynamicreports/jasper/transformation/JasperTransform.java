79c8ef23b878ca51b2493aeb1737bb17323d1d2a
Ricardo Mariaca <r.mariaca@dynamicreports.org>
2015-08-28 11:24:11 +0000

    added license

--- dynamicreports-core/src/main/java/net/sf/dynamicreports/jasper/transformation/JasperTransform.java
+++ dynamicreports-core/src/main/java/net/sf/dynamicreports/jasper/transformation/JasperTransform.java
@@ -1,3 +1,25 @@
+/**
+ * DynamicReports - Free Java reporting library for creating reports dynamically
+ *
+ * Copyright (C) 2010 - 2015 Ricardo Mariaca
+ * http://www.dynamicreports.org
+ *
+ * This file is part of DynamicReports.
+ *
+ * DynamicReports is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DynamicReports is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with DynamicReports. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package net.sf.dynamicreports.jasper.transformation;
 
 import java.util.Map;
@@ -10,6 +32,9 @@ import net.sf.dynamicreports.report.definition.ReportParameters;
 import net.sf.dynamicreports.report.exception.DRException;
 import net.sf.jasperreports.engine.design.JasperDesign;
 
+/**
+ * @author Ricardo Mariaca (r.mariaca@dynamicreports.org)
+ */
 public class JasperTransform implements JasperTransformAccessor {
 	private DRIDesignReport report;
 	private JasperReportDesign jasperReportDesign;
